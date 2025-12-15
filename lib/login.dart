5e4390a1b425059c7177f5f716d38e6286959875
Osei-Kuffour Kwaku <kuffuor679@gmail.com>
2025-12-15 10:33:53 +0000

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,8 @@
-import 'package:agriwealth/driverhome.dart';
+import 'package:agrich/driverhome.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:agriwealth/nav.dart';
+import 'package:agrich/nav.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
