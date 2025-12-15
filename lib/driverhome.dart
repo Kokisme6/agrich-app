b61b21f61c6790337ea4ad51dcf1fc618a4b5dfa
Osei-Kuffour Kwaku <kuffuor679@gmail.com>
2025-12-15 10:36:09 +0000

    Update import path for login.dart

--- lib/driverhome.dart
+++ lib/driverhome.dart
@@ -1,5 +1,5 @@
 
-import 'package:agriwealth/login.dart';
+import 'package:agrich/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
